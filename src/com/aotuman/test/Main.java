e0d99d69fcedf0b997f5e1761c7953b18eab8392
HuangKunTing <382968311@qq.com>
2021-03-28 23:17:50 +0800

    Initial commit

---
+++ src/com/aotuman/test/Main.java
@@ -0,0 +1,14 @@
+package com.aotuman.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Map<String, String> map = new HashMap<>();
+        map.put("aaa","1");
+        map.put("bbb","2");
+    }
+}
